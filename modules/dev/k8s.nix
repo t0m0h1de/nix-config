46451c0d5b3180e7e7e885575d979cf57bb264c2
Tomohide Sawada <Tomohide.Sawada@ibm.com>
2026-03-01 05:18:40 +0900

    feat: ansible関連のコマンドを追加

--- modules/dev/k8s.nix
+++ modules/dev/k8s.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    (python3.withPackages (ps: with ps; [
+      ps."ansible-core"
+      kubernetes
+      openshift
+      pyyaml
+    ]))
     kubectl
     kubernetes-helm
     argocd
