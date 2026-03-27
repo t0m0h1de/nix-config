b981c7f5ddf11e18abd15f8480b1db917d50dc7a
Tomohide Sawada <Tomohide.Sawada@ibm.com>
2026-03-27 09:57:34 +0900

    feat(vim): macOSの場合はvimは設定だけするように変更

--- modules/editors/vim.nix
+++ modules/editors/vim.nix
@@ -1,8 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  programs.vim = {
+  # Linux: Nixでvimをインストールして設定も管理
+  programs.vim = lib.mkIf pkgs.stdenv.isLinux {
     enable = true;
     defaultEditor = true;
     extraConfig = builtins.readFile ../../dotfiles/vimrc;
+  };
+
+  # Darwin: macOS組み込みvimを使うので~/.vimrcだけ配置
+  home.file.".vimrc" = lib.mkIf pkgs.stdenv.isDarwin {
+    source = ../../dotfiles/vimrc;
   };
 }
