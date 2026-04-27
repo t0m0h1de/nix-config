327622439c6244fab54198da071f5402dbffc1cd
Tomohide Sawada <ma1n.tmrev01ut10n@gmail.com>
2026-04-27 22:17:44 +0900

    chore: flake update

--- modules/shell/tools.nix
+++ modules/shell/tools.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    # TODO: Remove this override once direnv 2.38.0 is released, which includes a fix for the build failure on musl.
+    package = pkgs.direnv.overrideAttrs (_: {
+      doCheck = false;
+    });
   };
 }
